0b7ef26f35a610470757fb5b1bc98f674a242092
GiseRstewart <giserstewart@gmail.com>
2025-01-01 02:54:34 -0500

    agregue QuerySets y más

--- blog/models.py
+++ blog/models.py
@@ -4,10 +4,11 @@ from django.db import models
 from django.utils import timezone
 
 
+
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    content = models.TextField()
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
